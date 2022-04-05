1ac63711129147076fadf88a02c96ab458e8cce2
chana sat <77387934+sixsat@users.noreply.github.com>
2022-04-05 16:00:37 +0700

    Add and edit API route and view

--- webdev/urls.py
+++ webdev/urls.py
@@ -7,8 +7,10 @@ urlpatterns = [
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
-    path("resource/<int:resource_id>", views.resource_view, name="resource_view"),
+    path("<str:content_type>", views.content, name="content"),
+    path("resource/<int:resource_id>", views.resource, name="resource"),
 
-    # API Route
-    path("resource/<str:content_type>", views.resource, name="resource"),
+    # API Routes
+    path("getcontent/<str:content_type>", views.get_content, name="get_content"),
+    path("resource/<int:resource_id>/comment", views.comment, name="comment"),
 ]
--- webdev/views.py
+++ webdev/views.py
@@ -6,13 +6,144 @@ from django.core.paginator import Paginator
 from django.db import IntegrityError
 from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
+from django.template.defaulttags import register
 from django.urls import reverse
 
-from .models import Resource, User
+from .models import Comment, Resource, User
+
+CATEGORY = {
+    "book": {
+        "cap": "BOOK",
+        "view": "learn"
+    },
+    "code": {
+        "cap": "CODE",
+        "view": "practice"
+    },
+    "course": {
+        "cap": "CRS",
+        "view": "learn"
+    },
+    "document": {
+        "cap": "DOC",
+        "view": "learn"
+    },
+    "favorite": {
+        "cap": "FAV",
+        "view": "user"
+    },
+    "published": {
+        "cap": "PUB",
+        "view": "user"
+    },
+    "quiz": {
+        "cap": "QUIZ",
+        "view": "practice"
+    },
+    "video": {
+        "cap": "VID",
+        "view": "learn"
+    }
+}
+
+LEVEL = {
+    "INTR": {
+        "full": 'Introductory',
+        "color": 'text-success'
+    },
+    "MEDI": {
+        "full": 'Intermediate',
+        "color": 'text-warning'
+    },
+    "ADVC": {
+        "full": 'Advanced',
+        "color": 'text-danger'
+    }
+}
 
 
 def index(request):
     return render(request, "webdev/index.html")
+
+
+@login_required
+def comment(request, resource_id):
+
+    # Add a comment must be via POST
+    if request.method != "POST":
+        return JsonResponse({"error": "POST request required."}, status=400)
+
+    # Attempt to create new comment
+    comment = json.loads(request.body)["comment"]
+    try:
+        comment_obj = Comment(
+            user=request.user,
+            resource=Resource.objects.get(pk=resource_id),
+            comment=comment
+        )
+        comment_obj.save()
+    except Exception as e:
+        raise e
+
+    return JsonResponse({
+        "message": "Comment added.",
+        "username": request.user.username,
+        "timestamp": Comment.objects.get(pk=comment_obj.pk).ftime()
+    }, status=201)
+
+
+def content(request, content_type):
+
+    # Ensure valid content type
+    if content_type not in CATEGORY.keys():
+        return render(request, "webdev/content.html", {
+            "message": "Invalid content type."
+        }, status=404)
+
+    # To view 'favorite' and 'published', user must logged in
+    if not request.user.is_authenticated and content_type in ["favorite", "published"]:
+        return render(request, "webdev/content.html", {
+            "message": "Login required."
+        }, status=401)
+
+    if content_type == "favorite":
+        resources = Resource.objects.filter(favorite__pk=request.user.pk)
+    elif content_type == "published":
+        resources = Resource.objects.filter(user=request.user)
+    else:
+        resources = Resource.objects.filter(category=CATEGORY[content_type]["cap"])
+
+    # Return resources in reverse chronologial order
+    resources = resources.order_by("-timestamp")
+    return render(request, "webdev/content.html", {
+        "level": LEVEL,
+        "resources": resources,
+        "category": CATEGORY[content_type]
+    })
+
+
+def get_content(request, content_type):
+    if content_type not in CATEGORY.keys():
+        return JsonResponse({"error": "Invalid content type."}, status=404)
+
+    if not request.user.is_authenticated and content_type in ["favorite", "published"]:
+        return JsonResponse({"error": "Login required."}, status=401)
+
+    if content_type == "favorite":
+        resources = Resource.objects.filter(favorite__pk=request.user.pk)
+    elif content_type == "published":
+        resources = Resource.objects.filter(user=request.user)
+    else:
+        resources = Resource.objects.filter(category=CATEGORY[content_type]["cap"])
+
+    resources = resources.order_by("-timestamp")
+    return JsonResponse([resource.serialize() for resource in resources], safe=False)
+
+
+@register.filter
+def get_item(dictionary, key):
+    """Custom template filter to lookup dict with a variable"""
+    return dictionary.get(key)
 
 
 def login_view(request):
@@ -67,22 +198,18 @@ def register(request):
         return render(request, "webdev/register.html")
 
 
-def resource(request, content_type):
+def resource(request, resource_id):
 
-    # Ensure valid content type
-    if content_type not in ["BOOK", "CODE", "CRS", "DOC", "FAV", "PUB", "QUIZ", "VID"]:
-        return JsonResponse({"error": "Invalid content type"}, status=400)
+    # Query for requested resource
+    try:
+        resource = Resource.objects.get(pk=resource_id)
+        comments = Comment.objects.filter(resource=resource_id).order_by("timestamp")
+    except Resource.DoesNotExist:
+        return render(request, "webdev/resource.html", {
+            "message": "Resource does not exists."
+        })
 
-    if content_type == "FAV":
-        resources = Resource.objects.filter(favorite__contains=request.user)
-    elif content_type == "PUB":
-        resources = Resource.objects.filter(user=request.user)
-    else:
-        resources = Resource.objects.filter(category=content_type)
-
-    resources = resources.order_by("-timestamp")
-    return JsonResponse([resource.serialize() for resource in resources], safe=False)
-
-
-def resource_view(request, resource_id):
-    return render(request, "TODO")
+    return render(request, "webdev/resource.html", {
+        "resource": resource,
+        "comments": comments
+    })
