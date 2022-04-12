730dc21c0fedef6997b400bc47158494cbccb46b
chana sat <77387934+sixsat@users.noreply.github.com>
2022-04-12 16:35:59 +0700

    Add comment and form deletion feature

--- webdev/static/webdev/resource.js
+++ webdev/static/webdev/resource.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
-    resource_id = document.querySelector('#resource-id').value;
-    form_comment = document.querySelector('#comment-form');
-    form_like = document.querySelector('#like-form');
-    form_fave = document.querySelector('#fave-form');
+    const resource_id = document.querySelector('#resource-id').value;
+    const form_comment = document.querySelector('#comment-form');
+    const form_like = document.querySelector('#like-form');
+    const form_fave = document.querySelector('#fave-form');
 
     // If forms are not null, listen to form submission event
     if (form_comment && form_like && form_fave) {
@@ -10,6 +10,10 @@ document.addEventListener('DOMContentLoaded', function() {
         form_like.onsubmit = () => update(resource_id, 'like');
         form_fave.onsubmit = () => update(resource_id, 'fave');
     }
+
+    document.querySelectorAll('.del-comment-form').forEach(form => {
+        form.onsubmit = () => delete_comment(form);
+    });
 });
 
 function add_comment(resource_id) {
@@ -32,15 +36,20 @@ function add_comment(resource_id) {
     .then(response => response.json())
     .then(result => {
         const element = document.createElement('div');
-        element.classList.add('card', 'mb-3');
+        element.classList.add('card', 'mb-3', 'comment');
         element.innerHTML = `
             <div class="card-body">
                 <h5 class="card-title">${result['username']}</h5>
+                <form class="del-comment-form float-end" data-cid="${result['cid']}" method="post">
+                    <input type="hidden" name="csrfmiddlewaretoken" value="${csrftoken}">
+                    <button class="btn-none" type="submit"><i class="fas fa-trash-alt"></i></button>
+                </form>
                 <p class="card-text">${element_comment.value}</p>
                 <p class="card-text"><small class="text-muted">${result['timestamp']}</small></p>
             </div>
         `;
         document.querySelector('#comments').append(element);
+        element_comment.value = '';
 
         // Show response message
         const element_message = document.querySelector('#message');
@@ -54,14 +63,49 @@ function add_comment(resource_id) {
             element_message.innerHTML = result['message'];
         }
         element_message.innerHTML += '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>';
-    
-        element_comment.value = '';
+
+        // Update NodeList to include newly added comment
+        document.querySelectorAll('.del-comment-form').forEach(form => {
+            form.onsubmit = () => delete_comment(form);
+        });
     })
     .catch(error => {
         console.log(error);
     });
 
     // Prevent default submission
+    return false;
+}
+
+function delete_comment(element_form) {
+    const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
+    const element_comment = element_form.parentElement.parentElement;
+
+    fetch(`/resource/${resource_id}/comment`, {
+        method: 'POST',
+        headers: {'X-CSRFToken': csrftoken},
+        mode: 'same-origin',
+        body: JSON.stringify({
+            comment_id: element_form.dataset.cid
+        })
+    })
+    .then(response => response.json())
+    .then(result => {
+        if (result['error']) {
+            console.log(result)
+        } else {
+
+            // Delete the comment
+            element_comment.style.animationPlayState = 'running';
+            element_comment.addEventListener('animationend', () => {
+                element_comment.remove();
+            });
+        }
+    })
+    .catch(error => {
+        console.log(error);
+    });
+
     return false;
 }
 
